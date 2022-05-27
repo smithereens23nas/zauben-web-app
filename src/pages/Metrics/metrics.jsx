d3ec5a4fd01dc58581b62012dfcb08f79f2c2988
smiethereens23nas <nicholas23smith@gmail.com>
2022-05-26 21:56:44 -0400

    finished line graph

--- src/pages/Metrics/metrics.jsx
+++ src/pages/Metrics/metrics.jsx
@@ -11,13 +11,21 @@ function chartData() {
       datasets: [
         {
           label: 'My First dataset',
-          fillColor: 'rgba(220,220,220,0.2)',
+          fill: true,
+          backgroundColor: "rgb(15, 36, 52)",
           strokeColor: 'rgba(220,220,220,1)',
           pointColor: 'rgba(220,220,220,1)',
+          borderColor: "rgb(27, 132, 204)",
+          borderWidth: 7,
           pointStrokeColor: '#fff',
           pointHighlightFill: '#fff',
           pointHighlightStroke: 'rgba(220,220,220,1)',
           data: [65, 59, 80, 81, 56, 55, 40],
+          elements: {
+            point:{
+                radius: 0
+            }
+        }
         }
       ]
     }
@@ -39,13 +47,27 @@ function chartData() {
     datasetStrokeWidth: 2,
     datasetFill: true,
     borderColor: "blue",
+    scales: {
+        x: {
+          ticks: {
+            color: 'white'
+          }
+        },
+        y: {
+          ticks: {
+            color: 'white'
+          }
+        }
+      },
     legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
   }
   
   const styles = {
     graphContainer: {
       border: '1px solid black',
-      padding: '15px'
+      padding: '15px',
+      margin: "150px 100px",
+      
     }
   }
   
@@ -62,40 +84,27 @@ function chartData() {
       return (
         <div className="metric-show-page-bg">
         <div className="container-progress-card">
-          <div className="progress-card-item">
-            <div className="box">
-              <div className="percent-style">
-                <svg className="metric-svg">
-                  <circle cx="70" cy="70" r="70"></circle>
-                  <circle cx="72" cy="72" r="70"></circle>
-                </svg>
-                <div className="number-style">
-                  <h2>
-                    86<span>%</span>
-                  </h2>
-  
-                  <FiWind className="icon-style" />
-                </div>
-              </div>
+            
+
               <div>
                 <h2>
                   <a className="text-style" href="/performance">
                     Indoor Air Quality
                   </a>
                 </h2>
-              </div>
+            
             </div>
-            <p className="para-description">
+            <p className="metrics-description">
               Indoor air quality within and around buildings and structures. IAQ
               is known to affect the health, confort, and well-being of building
               occupants.
             </p>
-          </div>
+          
         </div>
         <div style={styles.graphContainer}>
           <Line data={this.state.data}
             options={options}
-            width="600" height="250"/>
+            width="500" height="175"/>
         </div>
         </div>
       )
