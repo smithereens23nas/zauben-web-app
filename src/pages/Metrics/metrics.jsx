c82c53cf1527da67ea5b6d4041c62b184fa26bc9
smiethereens23nas <nicholas23smith@gmail.com>
2022-05-31 19:57:27 -0400

    fixed date nav buttons

--- src/pages/Metrics/metrics.jsx
+++ src/pages/Metrics/metrics.jsx
@@ -56,9 +56,11 @@ function chartData() {
         },
         y: {
           ticks: {
-            color: 'white'
+            color: 'white',
+            beginAtZero: true
           }
-        }
+        },
+
       },
     legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
   }
