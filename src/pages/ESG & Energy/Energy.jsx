c94bee3f03f78bfc7fd72ffa09dccf0b5419cf9e
smiethereens23nas <nicholas23smith@gmail.com>
2022-05-31 21:56:23 -0400

    edits to energy and esg page

--- src/pages/ESG & Energy/Energy.jsx
+++ src/pages/ESG & Energy/Energy.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import DateNav from '../../components/DateNav/DateNav'
 
 
 const Energy = () => {
   return (
+      <div>
     <div className='energy-title-container'><h2>ESG & Energy</h2></div>
+    < DateNav />
+    <div className='energy-savings-container'>
+      <div>  <h3>Energy Savings</h3></div>
+      <div className='savings-items'>
+          <h4><span>75%</span>AC Savings</h4>
+          <h4><span>25%</span>Heat Savings</h4>
+          <h4><span>11%</span>Gas Savings</h4>
+          <h4><span>2%</span>Electric Savings</h4>
+          </div>
+
+        </div>
+        <div className='est-savings-container'><h3>Estimated Savings</h3></div>
+</div>
   )
 }
 
