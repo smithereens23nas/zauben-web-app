62a194d73fee5d5b3cb605037461c9845ae9b4ee
smiethereens23nas <nicholas23smith@gmail.com>
2022-05-31 21:32:02 -0400

    finished metrics show page - draft

--- src/components/DateNav/DateNav.jsx
+++ src/components/DateNav/DateNav.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 import "./DateNav.css";
+import { IoIosArrowBack } from "react-icons/io";
+
 
 const DateNav = () => {
-  const [activeNav, setActiveNav] = useState('#');
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <div className="btn-container">
+      <a href="/metricsDashboard" className="back-arrow">< IoIosArrowBack/></a>
       <div className="date-btns">
-      <button onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
-        <a href="#" >Today</a>
+        <button
+          onClick={() => setActiveNav("#")}
+          className={`nav-btn ${activeNav === "#" ? "active" : ""}`}
+        >
+          <a href="#">Today</a>
         </button>
-        <button onClick={() => setActiveNav('#week')} className={activeNav === '#week' ? 'active' : ''}>
-        <a href="#week" >Week</a>
+        <button
+          onClick={() => setActiveNav("#week")}
+          className={`nav-btn ${activeNav === "#week" ? "active" : ""}`}
+        >
+          <a href="#week">Week</a>
         </button>
-        <button onClick={() => setActiveNav('#month')} className={activeNav === '#month' ? 'active' : ''}>
-        <a href="#month" >Month</a>
+        <button
+          onClick={() => setActiveNav("#month")}
+          className={`nav-btn ${activeNav === "#month" ? "active" : ""}`}
+        >
+          <a href="#month">Month</a>
         </button>
-        <button onClick={() => setActiveNav('#year')} className={activeNav === '#year' ? 'active' : ''}>
-        <a href="#year" >Year</a>
+        <button
+          onClick={() => setActiveNav("#year")}
+          className={`nav-btn ${activeNav === "#year" ? "active" : ""}`}
+        >
+          <a href="#year">Year</a>
         </button>
       </div>
     </div>
   );
 };
 
-export default DateNav; 
+export default DateNav;
--- src/pages/Metrics/metrics.jsx
+++ src/pages/Metrics/metrics.jsx
@@ -4,117 +4,118 @@ import { Line } from "react-chartjs-2";
 import { Chart, Filler } from "chart.js";
 import { MdOutlineDateRange } from "react-icons/md";
 import { FiWind } from "react-icons/fi";
+import { IoIosArrowBack } from "react-icons/io";
 import DateNav from "../../components/DateNav/DateNav";
 
+
 function chartData() {
-    return {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          fill: true,
-          backgroundColor: "rgb(15, 36, 52)",
-          strokeColor: 'rgba(220,220,220,1)',
-          pointColor: 'rgba(220,220,220,1)',
-          borderColor: "rgb(27, 132, 204)",
-          borderWidth: 7,
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          elements: {
-            point:{
-                radius: 0
-            }
-        }
-        }
-      ]
-    }
-  }
-  
-  const options = {
-    scaleShowGridLines: true,
-    scaleGridLineColor: 'rgba(0,0,0,.05)',
-    scaleGridLineWidth: 1,
-    scaleShowHorizontalLines: true,
-    scaleShowVerticalLines: true,
-    bezierCurve: true,
-    bezierCurveTension: 0.4,
-    pointDot: true,
-    pointDotRadius: 4,
-    pointDotStrokeWidth: 1,
-    pointHitDetectionRadius: 20,
-    datasetStroke: true,
-    datasetStrokeWidth: 2,
-    datasetFill: true,
-    borderColor: "blue",
-    scales: {
-        x: {
-          ticks: {
-            color: 'white'
-          }
+  return {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fill: true,
+        backgroundColor: "rgb(15, 36, 52)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        borderColor: "rgb(27, 132, 204)",
+        borderWidth: 7,
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        elements: {
+          point: {
+            radius: 0,
+          },
         },
-        y: {
-          ticks: {
-            color: 'white',
-            beginAtZero: true
-          }
-        },
-
       },
-    legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-  }
-  
-  const styles = {
-    graphContainer: {
-      border: '1px solid black',
-      padding: '15px',
-      margin: "150px 100px",
-      
-    }
-  }
-  
-  class Metrics extends React.Component {
-  
-    constructor(props) {
-      super(props)
-      this.state = {
-        data: chartData()
-      }
-    }
-  
-    render() {
-      return (
-        <div className="metric-show-page-bg">
-          <DateNav />
-        <div className="container-progress-card">
-            
+    ],
+  };
+}
 
-              <div>
-                <h2>
-                  <a className="text-style" href="/performance">
-                    Indoor Air Quality
-                  </a>
-                </h2>
-            
-            </div>
-            <p className="metrics-description">
-              Indoor air quality within and around buildings and structures. IAQ
-              is known to affect the health, confort, and well-being of building
-              occupants.
-            </p>
-          
+const options = {
+  scaleShowGridLines: true,
+  scaleGridLineColor: "rgba(0,0,0,.05)",
+  scaleGridLineWidth: 1,
+  scaleShowHorizontalLines: true,
+  scaleShowVerticalLines: true,
+  bezierCurve: true,
+  bezierCurveTension: 0.4,
+  pointDot: true,
+  pointDotRadius: 4,
+  pointDotStrokeWidth: 1,
+  pointHitDetectionRadius: 20,
+  datasetStroke: true,
+  datasetStrokeWidth: 2,
+  datasetFill: true,
+  borderColor: "blue",
+  scales: {
+    x: {
+      ticks: {
+        color: "white",
+      },
+    },
+    y: {
+      ticks: {
+        color: "white",
+        beginAtZero: true,
+      },
+    },
+  },
+  legendTemplate:
+    '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+};
+
+const styles = {
+  graphContainer: {
+    border: "1px solid black",
+    padding: "15px",
+    margin: "50px 100px 0",
+  },
+};
+
+class Metrics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: chartData(),
+    };
+  }
+
+  render() {
+    return (
+      <div className="metric-show-page-bg">
+        
+        <DateNav />
+        <div className="container-progress-card">
+          <div>
+            <h2>
+              <a className="text-style" href="/performance">
+                Indoor Air Quality
+              </a>
+            </h2>
+          </div>
+          <p className="metrics-description">
+            Indoor air quality within and around buildings and structures. IAQ
+            is known to affect the health, confort, and well-being of building
+            occupants.
+          </p>
         </div>
         <div style={styles.graphContainer}>
-          <Line data={this.state.data}
+          <Line
+            data={this.state.data}
             options={options}
-            width="500" height="175"/>
+            width="500"
+            height="175"
+          />
         </div>
+        <div className="download-data-container">
+        <button className="download-data-btn">Download My Data</button>
         </div>
-        
-      )
-    }
+      </div>
+    );
   }
-
+}
 
 export default Metrics;
